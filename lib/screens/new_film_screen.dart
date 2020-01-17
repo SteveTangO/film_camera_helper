754f94a77b70866da975ab5639d6e8b324426a5e
SteveTangO <stevetanginchengdu@gmail.com>
2020-01-17 14:35:20 +0800

    Added new_film_screen and film stock buttons. Specified some important classes.

---
+++ lib/screens/new_film_screen.dart
@@ -0,0 +1,13 @@
+import 'package:film_camera_campanion/utilities/filmstock.dart';
+import 'package:flutter/material.dart';
+
+class NewFilmScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Film'),
+      ),
+    );
+  }
+}
