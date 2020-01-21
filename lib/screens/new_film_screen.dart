b91585c1dd00110c9a039446180c0c2ffa73f8a1
SteveTangO <stevetanginchengdu@gmail.com>
2020-01-21 18:23:18 +0800

    background animation with flare

--- lib/screens/new_film_screen.dart
+++ lib/screens/new_film_screen.dart
@@ -1,6 +1,10 @@
 import 'package:film_camera_campanion/utilities/constants.dart';
 import 'package:film_camera_campanion/utilities/filmstock.dart';
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:flare_dart/actor.dart';
+
+import '../utilities/constants.dart';
 
 class NewFilmScreen extends StatelessWidget {
   static String id = 'NewFilmScreen';
@@ -10,65 +14,92 @@ class NewFilmScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: kkodakyellow,
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: SizedBox(),
-              flex: 1,
+      body: Stack(
+        children: <Widget>[
+          SizedBox.expand(
+            child: Container(
+              color: kkodakyellow,
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Text(
-                      "New",
-                      style: TextStyle(
-                          color: Colors.red,
-                          fontSize: 40,
-                          fontWeight: FontWeight.w800),
-                    ),
-                    Text(
-                      "Film Roll",
-                      style: TextStyle(
-                          color: Colors.red,
-                          fontSize: 40,
-                          fontWeight: FontWeight.w800),
-                    )
-                  ],
+          ),
+          Positioned(
+            top: 500,
+            child: Container(
+              child: Animationss(),
+              height: 420,
+              width: 420,
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: SizedBox(),
+                  flex: 1,
                 ),
-              ),
-              flex: 3,
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Text(
+                          "New",
+                          style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 40,
+                              fontWeight: FontWeight.w800),
+                        ),
+                        Text(
+                          "Film Roll",
+                          style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 40,
+                              fontWeight: FontWeight.w800),
+                        )
+                      ],
+                    ),
+                  ),
+                  flex: 3,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[Text("One"), Text("Two")],
+                  ),
+                  flex: 3,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Text("mm"),
+                      Text("Film Name"),
+                      Text("Serial Number")
+                    ],
+                  ),
+                  flex: 3,
+                ),
+              ],
             ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[Text("One"), Text("Two")],
-              ),
-              flex: 3,
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Text("mm"),
-                  Text("Film Name"),
-                  Text("Serial Number")
-                ],
-              ),
-              flex: 3,
-            ),
-            Expanded(
-              child: SizedBox(),
-              flex: 1,
-            )
-          ],
-        ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class Animationss extends StatefulWidget {
+  @override
+  _AnimationState createState() => _AnimationState();
+}
+
+class _AnimationState extends State<Animationss> {
+  @override
+  Widget build(BuildContext context) {
+    return new FlareActor(
+      'images/kodakbackground.flr',
+      animation: "Untitled",
     );
   }
 }
