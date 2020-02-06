1f2891f57801a53d727861eb60e530439e154e35
SteveTangO <stevetanginchengdu@gmail.com>
2020-02-06 23:00:28 +0800

    re-scale new_film_screen

--- lib/screens/new_film_screen.dart
+++ lib/screens/new_film_screen.dart
@@ -6,6 +6,9 @@ import 'package:flutter/material.dart';
 import 'package:flare_dart/actor.dart';
 import 'package:film_camera_campanion/utilities/filmstock.dart';
 import '../utilities/constants.dart';
+import 'package:film_camera_campanion/utilities/size_config.dart';
+
+// re-sizing in completed in this page
 
 String filmsize = "35mm";
 
@@ -22,7 +25,9 @@ class NewFilmScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
+      resizeToAvoidBottomPadding: false, // super useful
       body: Stack(
         children: <Widget>[
           SizedBox.expand(
@@ -45,119 +50,123 @@ class NewFilmScreen extends StatelessWidget {
 //            ],
 //          ),//just for playing
           Positioned(
-            top: 500,
+            top: SizeConfig.blockSizeVertical * 55,
             child: Container(
               child: Animationss(),
-              height: 420,
-              width: 420,
+              height: SizeConfig.screenWidth,
+              width: SizeConfig.screenWidth,
             ),
           ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  child: SizedBox(),
-                  flex: 1,
-                ),
-                Expanded(
-                  child: Padding(
+          SafeArea(
+            child: Container(
+              width: SizeConfig.screenWidth,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: SizeConfig.safeBlockVertical * 3,
+                  ),
+                  Container(
                     padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      child: TextField(
-                        maxLines: 2,
-                        decoration: InputDecoration(
-                          hintText: 'Film Name',
-                          hintStyle: TextStyle(
-                              color: kkodakred,
-                              fontWeight: FontWeight.w900,
-                              fontSize: 64),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                          ),
-                        ),
-                        style: TextStyle(
+                    height: SizeConfig.safeBlockVertical * 30,
+                    child: TextField(
+                      //TODO finish the type process of text input  done
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.done,
+                      textCapitalization: TextCapitalization.characters,
+                      maxLines: 2,
+                      decoration: InputDecoration(
+                        hintText: 'Film Name',
+                        hintStyle: TextStyle(
                             color: kkodakred,
                             fontWeight: FontWeight.w900,
-                            fontSize: 64),
-                        onChanged: (value) {
-                          print(value);
-                        },
-                        cursorColor: kkodakred,
-                        autofocus: false,
+                            fontSize: SizeConfig.safeBlockHorizontal * 16),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        ),
                       ),
+                      style: TextStyle(
+                          color: kkodakred,
+                          fontWeight: FontWeight.w900,
+                          fontSize: 64),
+                      onChanged: (value) {
+                        print(value);
+                      },
+                      cursorColor: kkodakred,
+                      autofocus: false,
                     ),
                   ),
-                  flex: 2,
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      Text((filmiso).toString()),
-                      Text((picsperfilm).toString()),
-                    ],
+                  Container(
+                    height: SizeConfig.safeBlockVertical * 28,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Text((filmiso).toString()),
+                        Text((picsperfilm).toString()),
+                      ],
+                    ),
                   ),
-                  flex: 2,
-                ),
-                Expanded(
-                  flex: 3,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 20),
-                        child: Text(
-                          "New Film",
-                          style: TextStyle(
-                              color: kkodakyellow,
-                              fontSize: 40,
-                              fontWeight: FontWeight.w800),
+                  Container(
+                    height: SizeConfig.safeBlockVertical * 39,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Container(
+                          height: SizeConfig.safeBlockVertical * 7,
+                          margin: EdgeInsets.symmetric(horizontal: 20),
+                          child: Text(
+                            "New Film",
+                            style: TextStyle(
+                                color: kkodakyellow,
+                                fontSize: SizeConfig.safeBlockHorizontal * 10,
+                                fontWeight: FontWeight.w800),
+                          ),
                         ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 20),
-                        child: Text(
-                          "Format",
-                          style: TextStyle(
-                              color: kkodakred,
-                              fontSize: 40,
-                              fontWeight: FontWeight.w800),
+                        Container(
+                          height: SizeConfig.safeBlockVertical * 7,
+                          margin: EdgeInsets.symmetric(horizontal: 20),
+                          child: Text(
+                            "Format",
+                            style: TextStyle(
+                                color: kkodakred,
+                                fontSize: SizeConfig.safeBlockHorizontal * 10,
+                                fontWeight: FontWeight.w800),
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      ChooseFilmSize(),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 20),
-                        child: Text(
-                          "NO.$filmStockNo",
-                          style: TextStyle(
-                              color: kkodakyellow,
-                              fontSize: 40,
-                              fontWeight: FontWeight.w800),
+                        SizedBox(
+                          height: SizeConfig.safeBlockVertical * 1,
                         ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Container(
-                              margin: EdgeInsets.symmetric(horizontal: 50),
-                              child: IconButtonReturn(
-                                filmStockNo: filmStockNo,
-                                filmiso: filmiso,
-                                filmsize: filmsize,
-                              )),
-                        ],
-                      ),
-                    ],
+                        ChooseFilmSize(),
+                        SizedBox(
+                          height: SizeConfig.safeBlockVertical * 2,
+                        ),
+                        Container(
+                          height: SizeConfig.safeBlockVertical * 7,
+                          margin: EdgeInsets.symmetric(horizontal: 20),
+                          child: Text(
+                            "NO.$filmStockNo",
+                            style: TextStyle(
+                                color: kkodakyellow,
+                                fontSize: SizeConfig.safeBlockHorizontal * 10,
+                                fontWeight: FontWeight.w800),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            Container(
+                                margin: EdgeInsets.symmetric(horizontal: 50),
+                                child: IconButtonReturn(
+                                  filmStockNo: filmStockNo,
+                                  filmiso: filmiso,
+                                  filmsize: filmsize,
+                                )),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
@@ -195,55 +204,64 @@ class _ChooseFilmSizeState extends State<ChooseFilmSize> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      textBaseline: TextBaseline.alphabetic,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        Container(
-          width: 180,
-          margin: EdgeInsets.symmetric(horizontal: 20),
-          child: Text(
-            filmsize,
-            style: TextStyle(
-                color: Colors.white, fontSize: 40, fontWeight: FontWeight.w800),
+    return Container(
+      height: SizeConfig.safeBlockVertical * 6,
+      child: Row(
+        textBaseline: TextBaseline.alphabetic,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: SizeConfig.safeBlockHorizontal * 40,
+            margin: EdgeInsets.symmetric(
+                horizontal: SizeConfig.safeBlockHorizontal * 5), //20
+            child: Text(
+              filmsize,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: SizeConfig.safeBlockHorizontal * 9.5,
+                  fontWeight: FontWeight.w800),
+            ),
           ),
-        ),
-        SliderTheme(
-          data: SliderTheme.of(context).copyWith(
-            disabledThumbColor: Colors.white,
-            disabledInactiveTrackColor: kkodakyellow,
-            inactiveTickMarkColor: Colors.white,
-            inactiveTrackColor: kkodakyellow,
-            activeTrackColor: Colors.white,
-            thumbColor: kkodakyellow,
+          SliderTheme(
+            data: SliderTheme.of(context).copyWith(
+              disabledThumbColor: Colors.white,
+              disabledInactiveTrackColor: kkodakyellow,
+              inactiveTickMarkColor: Colors.white,
+              inactiveTrackColor: kkodakyellow,
+              activeTrackColor: Colors.white,
+              thumbColor: kkodakyellow,
 //            thumbShape: RoundSliderOverlayShape(overlayRadius: 20),
-            overlayColor: kkodakyellow,
+              overlayColor: kkodakyellow,
+            ),
+            child: Container(
+              width: SizeConfig.safeBlockHorizontal * 50,
+              child: Slider(
+                value: filmmeasure,
+                min: 1,
+                max: 5,
+                divisions: 4,
+                onChanged: (double newvalue) {
+                  print(newvalue);
+                  setState(() {
+                    filmmeasure = newvalue;
+                    if (newvalue == 1) {
+                      filmsize = '8 mm';
+                    } else if (newvalue == 2) {
+                      filmsize = '16 mm';
+                    } else if (newvalue == 3) {
+                      filmsize = '35 mm';
+                    } else if (newvalue == 4) {
+                      filmsize = 'MEDIUM';
+                    } else if (newvalue == 5) {
+                      filmsize = 'LARGE';
+                    }
+                  });
+                },
+              ),
+            ),
           ),
-          child: Slider(
-            value: filmmeasure,
-            min: 1,
-            max: 5,
-            divisions: 4,
-            onChanged: (double newvalue) {
-              print(newvalue);
-              setState(() {
-                filmmeasure = newvalue;
-                if (newvalue == 1) {
-                  filmsize = '8 mm';
-                } else if (newvalue == 2) {
-                  filmsize = '16 mm';
-                } else if (newvalue == 3) {
-                  filmsize = '35 mm';
-                } else if (newvalue == 4) {
-                  filmsize = 'MEDIUM';
-                } else if (newvalue == 5) {
-                  filmsize = 'LARGE';
-                }
-              });
-            },
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
@@ -270,7 +288,7 @@ class _IconButtonReturnState extends State<IconButtonReturn> {
           Icon(
             Icons.photo_camera,
             color: Colors.white,
-            size: 60,
+            size: SizeConfig.safeBlockHorizontal * 12,
           ),
         ],
       ),
